0a8ea79828fc108f4c55fb58beb195f271a09cff
vjiki <nezzabudka5@gmail.com>
2022-11-01 01:49:30 +0300

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import Application from './application';
+import reportWebVitals from './reportWebVitals';
+import './assets/css/dots.css'
+
+ReactDOM.render(
+    <BrowserRouter>
+      <Application />
+    </BrowserRouter>
+  ,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
